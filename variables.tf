f8f4d819c04d9b96fed68cd1f7956dfb09dab522
David Arnold <10138997+djaboxx@users.noreply.github.com>
2021-11-30 09:53:16 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "vpc_cidr" {
   default     = "10.0.0.0/16"
   description = "IP Address space of configured VPC"
-  type        = "string"
+  type        = string
 }
 
 variable "resource_tags" {
   description = "Optional map of tags to set on resources, defaults to empty map."
-  type        = "map"
+  type        = map
   default     = {}
 }
 
 variable "network_name" {
-  type        = "string"
+  type        = string
   description = "specify appropriate name of network"
 }
